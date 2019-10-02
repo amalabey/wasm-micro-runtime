2d45f4fcc3a1c234983a7b3ab974e168f927ca20
Amal Abeygunawardana <amalabey@gmail.com>
2019-10-02 21:20:11 +1000

    expose native api for linux

--- core/iwasm/products/linux/ext_lib_export.c
+++ core/iwasm/products/linux/ext_lib_export.c
@@ -15,7 +15,51 @@
  */
 
 #include "lib_export.h"
+#include "wasm_export.h"
+#include "native_interface.h"
+#include <stdlib.h>
+#include <stdio.h>
 
-static NativeSymbol extended_native_symbol_defs[] = { };
+
+void writeln(int buffer_offset, int size)
+{
+    // We need to convert the wasm memory relative address to absolute address in heap
+    // And then use Zephyr's printk to write to output or serial
+    wasm_module_inst_t module_inst = get_module_inst();
+    char *buffer = NULL;
+
+    if (!validate_app_addr(buffer_offset, size))
+        return;
+
+    buffer = addr_app_to_native(buffer_offset);
+
+    if (buffer != NULL) {
+        printf("%s\n", buffer);
+    }
+}
+
+void pinout(int pin, int state)
+{
+    // show gpio state on output
+    printf("GPIO pin: %d, set to %d \n", pin, state);
+}
+
+void thread_sleep(int millis)
+{
+    // sleep on linux
+    sleep(millis/1000);
+}
+
+void print_test(int num)
+{
+    printf("Hello from web assembly module: %d \n", num);
+}
+
+static NativeSymbol extended_native_symbol_defs[] = {
+    EXPORT_WASM_API(writeln),
+    EXPORT_WASM_API(pinout),
+    EXPORT_WASM_API(thread_sleep),
+    EXPORT_WASM_API(print_test)
+};
 
 #include "ext_lib_export.h"
